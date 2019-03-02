f7664c5fcb15f135f1287cff0fafd2f1b519c028
PanosIs <panoslantavos@gmail.com>
2019-03-02 16:40:26 +0200

    Implement initial graph class

---
+++ src/network/graph.py
@@ -0,0 +1,17 @@
+import numpy
+
+class Draft_Graph_Network:
+    def __init__(self, hero_pool_size : int, learning_rate : float):
+        self.connections = numpy.full(shape=(hero_pool_size, hero_pool_size), fill_value=0.5)
+        numpy.fill_diagonal(self.connections, val=0)
+
+        self.learning_rate = learning_rate
+
+    def train_single_example(self, picks : list, win : bool):
+        nonzero = numpy.outer(picks, picks)
+        if(not win):
+            nonzero *= -1
+        self.connections += self.learning_rate * nonzero
+
+    def get_connections(self):
+        return self.connections
